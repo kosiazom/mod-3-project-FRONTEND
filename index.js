16010905f99ceda5badc58456395160f3c5d57a6
JinaStrickland <Jina30@gmail.com>
2020-09-08 15:41:48 -0400

    added indexhtml, indexjs and stylecss

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-console.log("Frontend")
+const question = document.getElementById('question')
+const answer = document.getElementById('answer')
+const nextBtn = document.getElementById('next-button')
+const cardsURL = "http://localhost:3000/cards/"
 
-fetch("http://localhost:3000/cards")
+fetch(cardsURL)
 .then(res => res.json())
-.then(console.log)
+.then(questions => allQs(questions))
 
-fetch("http://localhost:3000/cards", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        front_side: "",
-        back_side: "",
-        category: "",
-        deck_id: d1.id
-    })
-})
-.then(res => res.json())
-.then(console.log)
+function allQs(questions) {
+
+}
+
+
+question.innerText = questions.front_side
+answer.innerText = questions.back_side
+
+// answer.innerText = questions.back_side
+
+// fetch("http://localhost:3000/cards", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         front_side: "What's ",
+//         back_side: "",
+//         category: "",
+//         deck_id: 1
+//     })
+// })
+// .then(res => res.json())
+// .then(console.log)
+
+
+
+
+
+
+
