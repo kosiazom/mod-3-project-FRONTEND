1fd0fe7295e17c88ae1f157f4ebcba5b5b4aaec0
Kosi Akporji <kosiazom@gmail.com>
2020-09-08 22:31:13 -0400

    added listener to javascript

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 
 const cardsURL = "http://localhost:3000/cards/"
 
+
+let js = document.getElementById('javascript')
+js.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e.target)
+
 fetch(cardsURL)
 .then(res => res.json())
 .then(cards => allQs(cards))
@@ -10,20 +16,58 @@ cards.forEach(card => eachCard(card))
 }
 
 function eachCard(card) {
+    let javaContainer = document.getElementById("main-container")
 
+    let divCard = document.createElement('div')
+    divCard.className = "card"
+
+    let frontDiv = document.createElement('div')
+    frontDiv.className = "the-front"
+
+    let h2 = document.createElement('h2')
+    h2.innerText = "Question"
     
+    let frontp = document.createElement('p')
+    frontp.id = "question-front"
+    frontp.innerText = card.front_side
 
-const qust = document.getElementById('question')
-qust.innerText = card.front_side
-const answer = document.getElementById('answer')
-answer.innerText = card.back_side
+    let nextBtn = document.createElement('button')
+    nextBtn.id = "next-button"
+    nextBtn.innerText = "Next"
 
-const nextBtn = document.getElementById('next-button')
+    let backDiv = document.createElement('div')
+    backDiv.className = "the-back"
+
+    let backH2 = document.createElement('h2')
+    backH2.innerText = "Answer"
+
+    let backp = document.createElement('p')
+    backp.innerText = card.back_side
+
+    frontDiv.append(h2, frontp, nextBtn)
+    backDiv.append(backH2, backp)
+
+    divCard.append(frontDiv, backDiv)
+
+    javaContainer.append(divCard)
+}
+})
+
+
+
+// const front = document.getElementById('question-front')
+// front.innerText = card.front_side
+// const back = document.getElementById('answer-back')
+// back.innerText = card.back_side
+
+// const nextBtn = document.getElementById('next-button')
+
+
 
    
 //when we add a listener to the next button it should fgot the next question and answer
     
-}
+
 
 // function sideBar() {
 
